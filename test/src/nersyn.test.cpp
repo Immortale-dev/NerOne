ed9308a70154ff16fc90383a36eec29faeda0846
Immortale <immortale.dev@gmail.com>
2022-02-20 16:57:23 +0000

    NerSyn tests

---
+++ test/src/nersyn.test.cpp
@@ -0,0 +1,48 @@
+DESCRIBE("NerSyn", {
+	using namespace nerone;
+	
+	NerSyn* syn;
+	
+	DESCRIBE("initialize default constructor instance", {
+		BEFORE_EACH({
+			syn = new NerSyn();
+		});
+		AFTER_EACH({
+			delete syn;
+		});
+		
+		IT("`get_node` should return `nullptr`", {
+			EXPECT(syn->get_node()).toBeNullPtr();
+		});
+		
+		IT("setting weight should return this weight with `get_weight`", {
+			syn->set_weight(12.45);
+			
+			EXPECT(syn->get_weight()).toBe(12.45);
+		});
+		
+		IT("setting the note, should return this node with `get_node`", {
+			auto node = std::make_shared<NerNode>();
+			syn->set_node(node);
+			
+			EXPECT(syn->get_node()).toBe(node);
+		});
+	});
+	
+	DESCRIBE("initialize constructor with weight and node", {
+		BEFORE_EACH({
+			syn = new NerSyn(std::make_shared<NerNode>(), 43.21);
+		});
+		AFTER_EACH({
+			delete syn;
+		});
+		
+		IT("should return 43.21 when `get_weight` is called", {
+			EXPECT(syn->get_weight()).toBe(43.21);
+		});
+		
+		IT("should not return nullptr when get_node is called", {
+			EXPECT(syn->get_node()).NOT().toBeNullPtr();
+		});
+	});
+});
--- test/test.cpp
+++ test/test.cpp
@@ -5,6 +5,7 @@
 
 SCENARIO_START
 #include "src/nernode.test.cpp"
+#include "src/nersyn.test.cpp"
 SCENARIO_END
 
 int main(){ return 0; }
