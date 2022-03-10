6c89d13b8e2cf5096b48d0a3d603b8853808ac79
Immortale <immortale.dev@gmail.com>
2022-03-10 21:44:25 +0000

    extend base multiplier class with the ability to use user provided class for computation accelerations

--- src/base_multiplier.hpp
+++ src/base_multiplier.hpp
@@ -6,18 +6,78 @@
 #include "nerlayer.hpp"
 #include "nercluster.hpp"
 #include "matrix.hpp"
+#include "base_calculator.hpp"
 
 namespace nerone {
 	
 	namespace multipliers {
 	
+		template<typename O = BaseCalculator>
 		class BaseMultiplier {
 			public:
 				void operator () (shared_cluster_t& cluster, value_list_t&& values);
+			
+			private:
+				using v_mat_mul_t = typename O::vector_matrix_multiplication<value_t>;
 		};
 		
 	}
 	
+}
+
+template<typename O>
+void nerone::multipliers::BaseMultiplier<O>::operator () (shared_cluster_t& cluster, value_list_t&& values) {
+	layer_list_t& layers = cluster->get_layers();
+	
+	value_list_t vals = std::move(values);
+	
+	size_t ind=1;
+	do{
+		node_list_t& nodes = layers[ind]->get_nodes();
+		
+		node_list_t& prev_nodes = layers[ind-1]->get_nodes();
+		for(size_t i=0;i<vals.size();i++){
+			prev_nodes[i]->set_value(vals[i]);
+			vals[i] = prev_nodes[i]->get_output();
+		}
+
+		if(ind >= layers.size()) break;
+		
+		// Add bias to the list of values
+		if(layers[ind-1]->get_bias()){
+			vals.push_back(layers[ind-1]->get_bias()->get_value());
+		}
+		
+		Matrix<value_t, v_mat_mul_t> b_vals({vals});
+		
+		vector<vector<value_t>> m_t_vals(nodes.size());
+
+		int j=0;
+		for(auto& node : nodes) {
+			syn_list_t& syns = node->get_syns();
+			vector<value_t> l_vals(syns.size());
+			
+			int i=0;
+			for(auto& syn : syns) {
+				l_vals[i++] = syn->get_weight();
+			}
+			
+			m_t_vals[j++] = std::move(l_vals);
+		}
+
+		Matrix<value_t, v_mat_mul_t> m_vals(std::move(m_t_vals));
+		
+		// Transpose for correct matrix multiplication
+		m_vals.transpose();
+
+		// Result of matrix multiplication should be [[x,x,x,x,x,x,...]]
+		Matrix<value_t, v_mat_mul_t> res = b_vals * m_vals;
+
+		vals.resize(res.get_cols());
+		for(size_t i=0;i<res.get_cols();i++){
+			vals[i] = res.get(0,i);
+		}
+	}while(ind++<layers.size());
 }
 
 #endif // NN_BASE_MULTIPLIER
